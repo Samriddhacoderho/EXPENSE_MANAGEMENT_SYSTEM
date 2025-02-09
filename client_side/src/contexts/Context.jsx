fd3ddd88a2f279bc2b2248df93a9db06bc0890ef
keshabbhattarai126 <keshab126bhattarai@gmail.com>
2025-02-09 13:20:19 +0545

    use state dark mode preparation on context.jsx

--- client_side/src/contexts/Context.jsx
+++ client_side/src/contexts/Context.jsx
@@ -4,15 +4,21 @@ import React from 'react'
 export const context=createContext()
 
 const Context = (props) => {
+  const [updateItems,setupdateItems]=useState(null)
+
   const [userMessage,setUserMessage]=useState(()=>{
     return localStorage.getItem("firstname") || "User"
   })
 
+  const [mode,setmode]=useState("Enable Dark Mode")
+
+
   useEffect(()=>{
     localStorage.setItem("firstname",userMessage)
   },[userMessage])
+
   return (
-     <context.Provider value={{userMessage,setUserMessage}}>
+     <context.Provider value={{userMessage,setUserMessage,updateItems,setupdateItems,mode,setmode}}>
         {props.children}
     </context.Provider>
   )
