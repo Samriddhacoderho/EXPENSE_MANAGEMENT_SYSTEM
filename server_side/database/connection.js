9c4ad325354d168c507e9e932de28d4095ab3292
samuelbasnet <samueallal743@gmail.com>
2025-02-10 11:18:25 +0545

    connect to db, with comments

--- server_side/database/connection.js
+++ server_side/database/connection.js
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 
 configDotenv()
 
-const uri=process.env.URI
+const uri=process.env.URI  //reference to environment
 
 const connectTo=async()=>{
-    await mongoose.connect(uri).then(()=>console.log("Succesufully Connected to Database")).catch((e)=>console.log(e.message))
+    await mongoose.connect(uri).then(()=>console.log("Succesufully Connected to Database")).catch((e)=>console.log(e.message)) //Connection message
 }
 
 export default connectTo
