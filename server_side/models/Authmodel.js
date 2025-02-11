c28637d3c7810c3f0fba6c577fa5aaaf577189ab
keshabbhattarai126 <keshab126bhattarai@gmail.com>
2025-02-11 12:09:43 +0545

    mongoose schema for collection, validation for data integrity

--- server_side/models/Authmodel.js
+++ server_side/models/Authmodel.js
@@ -18,7 +18,7 @@ const authSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
-    validate:[validator.isEmail,"Provide a proper Email ID"]
+    validate:[validator.isEmail,"Provide a valid Email ID"]
   },
   password: {
     type: String,
@@ -37,16 +37,22 @@ const authSchema = new mongoose.Schema({
   }
 });
 
+// Mongoose Middleware (pre-save hook)
+// This function runs before saving the document to the database.
 authSchema.pre("save",async function(next){
-    if(!this.isModified("password")) {return next()}
+    if(!this.isModified("password")) {
+      // If the password hasn't been modified (like during profile update without password change), skip hashing.
+      return next()}
     else
-    {
+    { // If the password is new or modified:
     this.password=await bcrypt.hash(this.password,10)
+    // Hash the password using bcrypt with a salt round of 10 for security.
     this.confirmPass=undefined
     }
 })
 
 const authModel=mongoose.model("logins",authSchema)
+// This creates a 'logins' collection in MongoDB (or uses it if already exists) based on the authSchema structure.
 
 export default authModel
 
